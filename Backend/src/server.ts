6dcd3244eeaff640f5d692d9b7b9c9c717acb7ea
Nikhil Raj Barnwal <nraj07054@gmail.com>
2026-02-14 12:39:42 +0530

    first commit

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -8,11 +8,12 @@ import {evaluateOffersController} from "./controllers/offer.controller.ts";
 
 const app: Application = express();
 const port: string | number = process.env.PORT || 8000;
+const frontendUrl = process.env.FRONTEND_URL || "http://localhost:3000";
 const SESSION_SECRET: string =
     process.env.SESSION_SECRET ?? "dev-secret";
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: frontendUrl,
     credentials: true
 }));
 
